c1e299c8f8bcc75fc256c06bb540e5ab7b22ad60
devfrank9 <devfrank9@gmail.com>
2022-07-17 18:48:35 +0900

    Feat: "react-gradle-mysql 초기 세팅"

---
+++ src/main/java/com/teamprj/nubigo/NubigoApplication.java
@@ -0,0 +1,13 @@
+package com.teamprj.nubigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamprj/nubigo/NubigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamprj.nubigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
