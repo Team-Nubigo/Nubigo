b4ce4eee00157e20f5453fb30860aa209cf758de
PJScript <kilo718@naver.com>
2022-07-19 20:47:26 +0900

    feat/layout: 기본 레이아웃 작성 및 라우팅

---
+++ client/src/pages/myPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from '../styles/common';
+
+const MyPage = () => {
+  return (
+    <S.Layout>
+      <S.Inner>MyPage</S.Inner>
+    </S.Layout>
+  );
+};
+
+export default MyPage;
