dfa7eae0f1bdd3183605af0b667dcc0e1e93d218
AiRn33 <tkdqhr6777@naver.com>
2022-07-26 00:08:07 +0900

    123455555

---
+++ src/main/java/com/teamprj/nubigo/controller/testController.java
@@ -0,0 +1,13 @@
+package com.teamprj.nubigo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world TEst ok";
+    }
+}
